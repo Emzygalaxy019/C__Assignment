81754a912cc3ffa761b5f7d4e62620cdfff9503b
Attiye Emmanuel <92821512+Emzygalaxy019@users.noreply.github.com>
2024-03-04 00:55:45 +0100

    source code uploaded

--- Program.cs
+++ Program.cs
@@ -1,46 +1,31 @@
 ﻿using System;
-namespace Sample_1
+namespace Question6
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            int i = 4;
-            double d = 4.0;
-            string s = "Polyilaro";
+            int number;
+            String message;
+            Console.WriteLine("Enter a number: ");
+             number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0) {
+                message = "The number is positive.";
+            }
+            else if (number < 0) {
+                message = "The number is negative.";
+            }
+            else
+            {
+                message = "The number is zero.";
+            }
+
+            Console.WriteLine("The number you entered is: " + number);
+            Console.WriteLine(message);
+
             
-            //Declaring additional variable
-            int i2;
-            double d2;
-            string s2;
-
-            // Read and initializing variable
-            Console.WriteLine("Enter an integer value");
-            i2 = 
-                Convert .ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter a double value");
-            d2 = 
-                Convert .ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter a string message");
-             s2 = Console.ReadLine();
-
-            // printing the sum of both inter value
-
-            Console.WriteLine(i + i2);
-
-            // printing the sum of the double value
-            double doublesum = d + d2;
-            Console.WriteLine(doublesum);
-
-            // Concatenating and printing the string variable
-            Console.WriteLine(s + s2);
-           
-
-
-
+ 
         }
-
     }
 }
