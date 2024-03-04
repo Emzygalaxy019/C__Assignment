29913a750e386f60288c529e399b2ca0a1122fcb
Attiye Emmanuel <92821512+Emzygalaxy019@users.noreply.github.com>
2024-03-04 01:10:40 +0100

    Question five source code

---
+++ Program source code 5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Question5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            float[] alpha = new float[50];
+            for (int i = 0; i < 25; i++) {
+                alpha[i] = i * i;
+            }
+
+            for (int i = 25; i < 50; i++)
+            {
+                alpha[i] = 3 * i;
+            }
+
+            Console.WriteLine("The array alpha is:");
+
+            for(int i = 0; i < 50; i++) {
+                Console.WriteLine(alpha[i] + " ");
+
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+
+            }
+           
+
+        }
+    }
+}
