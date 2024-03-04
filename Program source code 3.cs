72df1eec053102e02085d9887c921fd71bb576d1
Attiye Emmanuel <92821512+Emzygalaxy019@users.noreply.github.com>
2024-03-04 01:07:15 +0100

    Question three source code

---
+++ Program source code 3.cs
@@ -0,0 +1,18 @@
+﻿namespace Question3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           String text;
+            Console.WriteLine("Enter a string");
+            text = Console.ReadLine();
+            char[] charArray = text.ToCharArray();
+            Console.WriteLine("The string in uppercase is : ");
+            foreach (char c in charArray)
+            {
+                Console.Write(char.ToUpper (c));
+            }
+        }
+    }
+}
